f27789782cb92a1ac35be586e24ab01012cd266c
Kate <73944723+Kondratyyeva@users.noreply.github.com>
2022-07-30 12:51:14 +0300

    BookKeepingApplication added

---
+++ src/main/java/ru/kondratyeva/springcourse/BookkeepingApplication.java
@@ -0,0 +1,17 @@
+package ru.kondratyeva.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class BookkeepingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookkeepingApplication.class, args);
+    }
+   /* @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate();
+    }*/
+}
